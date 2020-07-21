64408eea42e68755d5ea093e0a65bdc1ce5fe7a8
Shani Kumar <52395212+Shani0045@users.noreply.github.com>
2020-07-21 17:43:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from googletrans import Translator
+from PIL import Image
+
+def main():    
+    st.sidebar.title("Developer Profile")
+    if st.sidebar.checkbox("Show/Hide"):
+        st.sidebar.markdown("Name : Shani Kumar")
+        st.sidebar.markdown("Profile: B.tech (Computer Science Engineer) ")
+        st.sidebar.subheader("Instagram Id")
+        st.sidebar.markdown("https://www.instagram.com/shanikumar02/")
+        img=Image.open("shanibhai.jpg")
+        st.sidebar.image(img,width=200,caption="Shani Kumar")
+    try:    
+        p="""<h1 style=text-align:center;background-color:wheat;color:black;border-radius:15px;padding-top:12px;margin-top:10px;>Welcome To Translator</h1>"""
+        st.markdown(p,unsafe_allow_html=True)
+
+        a=st.selectbox("Choose Source Language",['english', 'hindi', 'amharic', 'arabic', 'armenian', 'azerbaijani', 'basque', 'belarusian', 'bengali', 'bosnian', 'bulgarian', 'catalan', 'cebuano', 'chichewa', 'chinese (simplified)', 'chinese (traditional)', 'corsican', 'croatian', 'czech', 'danish', 'dutch', 'afrikaans', 'esperanto', 'estonian', 'filipino', 'finnish', 'french', 'frisian', 'galician', 'georgian', 'german', 'greek', 'gujarati', 'haitian creole', 'hausa', 'hawaiian', 'hebrew', 'hebrew', 'albanian', 'hmong', 'hungarian', 'icelandic', 'igbo', 'indonesian', 'irish', 'italian', 'japanese', 'javanese', 'kannada', 'kazakh', 'khmer', 'korean', 'kurdish (kurmanji)', 'kyrgyz', 'lao', 'latin', 'latvian', 'lithuanian', 'luxembourgish', 'macedonian', 'malagasy', 'malay', 'malayalam', 'maltese', 'maori', 'marathi', 'mongolian', 'myanmar (burmese)', 'nepali', 'norwegian', 'odia', 'pashto', 'persian', 'polish', 'portuguese', 'punjabi', 'romanian', 'russian', 'samoan', 'scots gaelic', 'serbian', 'sesotho', 'shona', 'sindhi', 'sinhala', 'slovak', 'slovenian', 'somali', 'spanish', 'sundanese', 'swahili', 'swedish', 'tajik', 'tamil', 'telugu', 'thai', 'turkish', 'ukrainian', 'urdu', 'uyghur', 'uzbek', 'vietnamese', 'welsh', 'xhosa', 'yiddish', 'yoruba', 'zulu'])
+        st.subheader("To")
+        b=st.selectbox("Choose Destination Language",['hindi', 'english', 'amharic', 'arabic', 'armenian', 'azerbaijani', 'basque', 'belarusian', 'bengali', 'bosnian', 'bulgarian', 'catalan', 'cebuano', 'chichewa', 'chinese (simplified)', 'chinese (traditional)', 'corsican', 'croatian', 'czech', 'danish', 'dutch', 'afrikaans', 'esperanto', 'estonian', 'filipino', 'finnish', 'french', 'frisian', 'galician', 'georgian', 'german', 'greek', 'gujarati', 'haitian creole', 'hausa', 'hawaiian', 'hebrew', 'hebrew', 'albanian', 'hmong', 'hungarian', 'icelandic', 'igbo', 'indonesian', 'irish', 'italian', 'japanese', 'javanese', 'kannada', 'kazakh', 'khmer', 'korean', 'kurdish (kurmanji)', 'kyrgyz', 'lao', 'latin', 'latvian', 'lithuanian', 'luxembourgish', 'macedonian', 'malagasy', 'malay', 'malayalam', 'maltese', 'maori', 'marathi', 'mongolian', 'myanmar (burmese)', 'nepali', 'norwegian', 'odia', 'pashto', 'persian', 'polish', 'portuguese', 'punjabi', 'romanian', 'russian', 'samoan', 'scots gaelic', 'serbian', 'sesotho', 'shona', 'sindhi', 'sinhala', 'slovak', 'slovenian', 'somali', 'spanish', 'sundanese', 'swahili', 'swedish', 'tajik', 'tamil', 'telugu', 'thai', 'turkish', 'ukrainian', 'urdu', 'uyghur', 'uzbek', 'vietnamese', 'welsh', 'xhosa', 'yiddish', 'yoruba', 'zulu'])
+        c=st.text_area("Message box",height=170)
+        if st.button("Translate"):
+            t=Translator()
+            result=t.translate(c,src=a,dest=b)
+            st.text_area("",result.text,height=180)
+    except:
+        pass
+
+
+    hide="""
+    <style>
+    #MainMenu{visibility:hidden;}
+    footer{visibility:hidden;}
+    </style>
+    """
+    st.markdown(hide,unsafe_allow_html=True)
+
+if __name__="__main__":
+    main()
